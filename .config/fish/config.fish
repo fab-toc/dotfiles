9f86e7c4cc04f6424d71a1e92dbcf64b87547145
Fabio <>
2024-07-02 23:59:14 +0200

    changed tide prompt config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,28 +21,6 @@ set -Ux NVM_DIR "$XDG_DATA_HOME/nvm"
 
 
 #### Customize Tide prompt colors
-set -Ux tide_git_bg_color 268bd2
-set -Ux tide_git_bg_color_unstable C4A000
-set -Ux tide_git_bg_color_urgent CC0000
-set -Ux tide_git_branch_color 000000
-set -Ux tide_git_color_branch 000000
-set -Ux tide_git_color_conflicted 000000
-set -Ux tide_git_color_dirty 000000
-set -Ux tide_git_color_operation 000000
-set -Ux tide_git_color_staged 000000
-set -Ux tide_git_color_stash 000000
-set -Ux tide_git_color_untracked 000000
-set -Ux tide_git_color_upstream 000000
-set -Ux tide_git_conflicted_color 000000
-set -Ux tide_git_dirty_color 000000
-set -Ux tide_git_icon 
-set -Ux tide_git_operation_color 000000
-set -Ux tide_git_staged_color 000000
-set -Ux tide_git_stash_color 000000
-set -Ux tide_git_untracked_color 000000
-set -Ux tide_git_upstream_color 000000
-
-set -Ux tide_pwd_bg_color 444444
 set -Ux tide_character_color 268bd2
 set -Ux tide_character_color_failure 'red'
 
@@ -61,6 +39,7 @@ alias maj "sudo apt full-upgrade; flatpak update; fisher update"
 
 
 
+
 # Abbreviations
 abbr -a gck git checkout
 abbr -a gpo git push origin
@@ -69,10 +48,11 @@ abbr -a gco git commit -m
 
 
 
+
 # Dev
 alias p pnpm
 
-# pnpm
+## pnpm
 set -gx PNPM_HOME "/home/ftocco/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
