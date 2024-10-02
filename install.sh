65b854e91834b103388b3b3dd50447a3659bd1f5
fab-toc <81991643+fab-toc@users.noreply.github.com>
2024-10-02 16:11:43 +0200

    added bat and eza cli

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 # Install all the packages needed
-sudo apt install zsh stow curl git neovim
+sudo apt install -y zsh stow curl git neovim bat gpg
 chsh -s $(which zsh)
 
+# Install eza cli
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 # Install Starship
 curl -sS https://starship.rs/install.sh | sh # follow the installation script but do NOT append to configuration
