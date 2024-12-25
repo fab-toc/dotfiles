55446183e6d4f364cde4b3938bd80a5c95727764
fab-toc <81991643+fab-toc@users.noreply.github.com>
2024-12-25 19:11:22 +0100

    added auto-install for zoxide

--- macOS-install.sh
+++ macOS-install.sh
@@ -16,21 +16,22 @@ ask() {
 # Install all the packages needed
 brew install curl stow neovim bat eza starship fzf
 
-
 starship preset no-runtime-versions -o ~/.config/starship.toml # apply starship preset
 
+# Install zoxide
+if ask "Do you want to install zoxide ?"; then
+  brew install zoxide
+fi
 
 # Install uv
 if ask "Do you want to install uv for Python ?"; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 
-
 # Install pnpm
 if ask "Do you want to install pnpm (package manager for js projects) ?"; then
   brew install pnpm
 fi
-
 
 # Apply my configuration
 cd ~/dotfiles
